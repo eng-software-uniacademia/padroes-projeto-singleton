e3a47eb52a65aa1a5457c396449f6c65567372d1
Natan Fernandes <natanfernandes@protonmail.com>
2023-03-12 18:20:19 -0300

    remove folders

--- src/main/java/SharedObject.java
+++ src/main/java/SharedObject.java
@@ -1,5 +1,3 @@
-package singleton;
-
 import lombok.Getter;
 import lombok.Setter;
 
--- src/test/java/SharedObjectTest.java
+++ src/test/java/SharedObjectTest.java
@@ -1,5 +1,3 @@
-package singleton;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
